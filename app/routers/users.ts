60d0c1a3a05d047190b4f2720dad468142cb53c6
Lami <ikki17617@gmail.com>
2023-12-22 04:44:17 +0900

    Feature: Add Account Data

--- app/routers/users.ts
+++ app/routers/users.ts
@@ -2,10 +2,16 @@ import {
   Hono,
   type Context,
 } from 'https://deno.land/x/hono@v3.11.7/mod.ts';
-import Twitter from '../internal/web_twitter.ts';
+import { LRUCache } from 'npm:lru-cache@10.1.0';
+import Twitter, { type User } from '../internal/web_twitter.ts';
 import SyndicationTwitter from '../internal/syndication_twitter.ts';
 
 const app = new Hono();
+
+const usersCache = new LRUCache<string, User>({
+  max: 1000,
+  ttl: 1000 * 60 * 60,
+});
 
 const twitter = new Twitter(Deno.env.get('X_AUTH_TOKEN'));
 const syndicationTwitter = new SyndicationTwitter(Deno.env.get('X_AUTH_TOKEN'));
@@ -36,37 +42,71 @@ app.get('/:username', async (c: Context) => {
       screen_name: username,
     })}`);
   }
-  const result = await twitter.getUserByScreenName(String(username));
-  // TODO: followers, following, liked を機能させるためにユーザーのresultをデータベースまたはキャッシュに保存する
+  const cachedUser = usersCache.get(String(username));
+  if (cachedUser) {
+    return c.json({
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'Person',
+      id: `${new URL(c.req.url).origin}/users/${cachedUser.legacy.screen_name}`,
+      name: cachedUser.legacy.name,
+      preferredUsername: cachedUser.legacy.screen_name,
+      // TODO: URLを展開したのは良いものの、URLとして認識されない。多分HTMLタグを使わなきゃいけない
+      // TODO: メンションが認識されていない
+      summary: expandDescription(cachedUser.legacy.description, cachedUser.legacy.entities.description.urls),
+      icon: {
+        type: 'Image',
+        url: cachedUser.legacy.profile_image_url_https.replace('_normal', ''),
+      },
+      image: cachedUser.legacy.profile_banner_url ? {
+        type: 'Image',
+        url: cachedUser.legacy.profile_banner_url,
+      } : undefined,
+      location: cachedUser.legacy.location ? {
+        type: 'Place',
+        name: cachedUser.legacy.location,
+      } : undefined,
+      url: cachedUser.legacy.url ? cachedUser.legacy.entities.url ? expandUrl(cachedUser.legacy.url, cachedUser.legacy.entities.url.urls) : cachedUser.legacy.url : undefined,
+      published: cachedUser.legacy.created_at.toISOString(),
+      // TODO: attachment プロパティを用いてProプロフィールや公式のURL、またリポジトリのURLを掲載(検討)する。
+      inbox: `${new URL(c.req.url).origin}/users/${cachedUser.legacy.screen_name}/inbox`,
+      outbox: `${new URL(c.req.url).origin}/users/${cachedUser.legacy.screen_name}/outbox`,
+      followers: `${new URL(c.req.url).origin}/users/${cachedUser.legacy.screen_name}/followers`,
+      following: `${new URL(c.req.url).origin}/users/${cachedUser.legacy.screen_name}/following`,
+      liked: `${new URL(c.req.url).origin}/users/${cachedUser.legacy.screen_name}/liked`,
+    }, 200, { 'content-type': 'application/activity+json' });
+  }
+  const user = await twitter.getUserByScreenName(String(username));
+  // LRU-Cache でテスト
+  usersCache.set(String(username), user);
   return c.json({
     '@context': 'https://www.w3.org/ns/activitystreams',
     type: 'Person',
-    id: `${new URL(c.req.url).origin}/users/${result.legacy.screen_name}`,
-    name: result.legacy.name,
-    preferredUsername: result.legacy.screen_name,
+    id: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}`,
+    name: user.legacy.name,
+    preferredUsername: user.legacy.screen_name,
     // TODO: URLを展開したのは良いものの、URLとして認識されない。多分HTMLタグを使わなきゃいけない
     // TODO: メンションが認識されていない
-    summary: expandDescription(result.legacy.description, result.legacy.entities.description.urls),
+    summary: expandDescription(user.legacy.description, user.legacy.entities.description.urls),
     icon: {
       type: 'Image',
-      url: result.legacy.profile_image_url_https.replace('_normal', ''),
+      url: user.legacy.profile_image_url_https.replace('_normal', ''),
     },
-    image: result.legacy.profile_banner_url ? {
+    image: user.legacy.profile_banner_url ? {
       type: 'Image',
-      url: result.legacy.profile_banner_url,
+      url: user.legacy.profile_banner_url,
     } : undefined,
-    location: result.legacy.location ? {
+    location: user.legacy.location ? {
       type: 'Place',
-      name: result.legacy.location,
+      name: user.legacy.location,
     } : undefined,
-    url: result.legacy.url ? result.legacy.entities.url ? expandUrl(result.legacy.url, result.legacy.entities.url.urls) : result.legacy.url : undefined,
-    published: result.legacy.created_at.toISOString(),
+    url: user.legacy.url ? user.legacy.entities.url ? expandUrl(user.legacy.url, user.legacy.entities.url.urls) : user.legacy.url : undefined,
+    published: user.legacy.created_at.toISOString(),
     // TODO: attachment プロパティを用いてProプロフィールや公式のURL、またリポジトリのURLを掲載(検討)する。
-    inbox: `${new URL(c.req.url).origin}/users/${result.legacy.screen_name}/inbox`,
-    outbox: `${new URL(c.req.url).origin}/users/${result.legacy.screen_name}/outbox`,
-    followers: `${new URL(c.req.url).origin}/users/${result.legacy.screen_name}/followers`,
-    following: `${new URL(c.req.url).origin}/users/${result.legacy.screen_name}/following`,
-    liked: `${new URL(c.req.url).origin}/users/${result.legacy.screen_name}/liked`,
+    inbox: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}/inbox`,
+    outbox: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}/outbox`,
+    followers: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}/followers`,
+    following: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}/following`,
+    liked: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}/liked`,
   }, 200, { 'content-type': 'application/activity+json' });
 });
 
@@ -89,7 +129,44 @@ app.get('/:username/outbox', async (c: Context) => {
   if (!c.req.header('accept')?.includes('application/activity+json')) {
     return c.text('400 Bad Request', 400);
   }
-  // TODO: outboxの実装
+  const userTimeline = await syndicationTwitter.timelineProfile(username);
+  if (!userTimeline.contextProvider.hasResults) {
+    return c.text('404 Not Found', 404);
+  }
+  const cachedUser = usersCache.get(String(username));
+  if (cachedUser) {
+    return c.json({
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'OrderedCollection',
+      id: `${new URL(c.req.url).origin}/users/${username}/outbox`,
+      totalItems: cachedUser.legacy.statuses_count,
+      // TODO: 何故かノートが表示されない
+      orderedItems: userTimeline.timeline.entries.map(({ content: { tweet } }) => ({
+        type: 'Note',
+        id: `${new URL(c.req.url).origin}/notes/${tweet.id_str}`,
+        actor: `${new URL(c.req.url).origin}/users/${username}`,
+        name: tweet.full_text,
+        content:tweet.full_text,
+      })),
+    }, 200, { 'content-type': 'application/activity+json' });
+  }
+  const user = await twitter.getUserByScreenName(String(username));
+  // LRU-Cache でテスト
+  usersCache.set(String(username), user);
+  return c.json({
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'OrderedCollection',
+    id: `${new URL(c.req.url).origin}/users/${user.legacy.screen_name}/outbox`,
+    totalItems: user.legacy.statuses_count,
+      // TODO: 何故かノートが表示されない
+      orderedItems: userTimeline.timeline.entries.map(({ content: { tweet } }) => ({
+      type: 'Note',
+      id: `${new URL(c.req.url).origin}/notes/${tweet.id_str}`,
+      actor: `${new URL(c.req.url).origin}/users/${username}`,
+      name: tweet.full_text,
+      content:tweet.full_text,
+    })),
+}, 200, { 'content-type': 'application/activity+json' });
 });
 
 app.get('/:username/followers', async (c: Context) => {
@@ -100,7 +177,24 @@ app.get('/:username/followers', async (c: Context) => {
   if (!c.req.header('accept')?.includes('application/activity+json')) {
     return c.redirect(`https://x.com/${username}/followers`);
   }
-  // TODO: followersの実装
+  const cachedUser = usersCache.get(String(username));
+  if (cachedUser) {
+    return c.json({
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'OrderedCollection',
+      id: `${new URL(c.req.url).origin}/users/${username}/followers`,
+      totalItems: cachedUser.legacy.followers_count,
+    }, 200, { 'content-type': 'application/activity+json' });
+  }
+  const user = await twitter.getUserByScreenName(String(username));
+  // LRU-Cache でテスト
+  usersCache.set(String(username), user);
+  return c.json({
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'OrderedCollection',
+    id: `${new URL(c.req.url).origin}/users/${username}/followers`,
+    totalItems: user.legacy.followers_count,
+  }, 200, { 'content-type': 'application/activity+json' });
 });
 
 app.get('/:username/following', async (c: Context) => {
@@ -111,7 +205,24 @@ app.get('/:username/following', async (c: Context) => {
   if (!c.req.header('accept')?.includes('application/activity+json')) {
     return c.redirect(`https://x.com/${username}/following`);
   }
-  // TODO: followingの実装
+  const cachedUser = usersCache.get(String(username));
+  if (cachedUser) {
+    return c.json({
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'OrderedCollection',
+      id: `${new URL(c.req.url).origin}/users/${username}/followers`,
+      totalItems: cachedUser.legacy.friends_count,
+    }, 200, { 'content-type': 'application/activity+json' });
+  }
+  const user = await twitter.getUserByScreenName(String(username));
+  // LRU-Cache でテスト
+  usersCache.set(String(username), user);
+  return c.json({
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'OrderedCollection',
+    id: `${new URL(c.req.url).origin}/users/${username}/followers`,
+    totalItems: user.legacy.friends_count,
+  }, 200, { 'content-type': 'application/activity+json' });
 });
 
 app.get('/:username/liked', async (c: Context) => {
@@ -122,7 +233,24 @@ app.get('/:username/liked', async (c: Context) => {
   if (!c.req.header('accept')?.includes('application/activity+json')) {
     return c.redirect(`https://x.com/${username}/likes`);
   }
-  // TODO: likedの実装
+  const cachedUser = usersCache.get(String(username));
+  if (cachedUser) {
+    return c.json({
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      type: 'OrderedCollection',
+      id: `${new URL(c.req.url).origin}/users/${username}/followers`,
+      totalItems: cachedUser.legacy.favourites_count,
+    }, 200, { 'content-type': 'application/activity+json' });
+  }
+  const user = await twitter.getUserByScreenName(String(username));
+  // LRU-Cache でテスト
+  usersCache.set(String(username), user);
+  return c.json({
+    '@context': 'https://www.w3.org/ns/activitystreams',
+    type: 'OrderedCollection',
+    id: `${new URL(c.req.url).origin}/users/${username}/followers`,
+    totalItems: user.legacy.favourites_count,
+  }, 200, { 'content-type': 'application/activity+json' });
 });
 
 export default app;
